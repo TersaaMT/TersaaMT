d9b59a52e69341b39e91bdfbe0b727bc747407c2
TersaMT <tersakarskiy@gmail.com>
2025-06-19 14:37:04 +0300

    Добавил кнопку MA Guard

--- app/strategies/ma-guard-bot/page.tsx/page.tsx
+++ app/strategies/ma-guard-bot/page.tsx/page.tsx
@@ -2,7 +2,7 @@ export default function MAGuardStrategy() {
   return (
     <main style={{ textAlign: "center", paddingTop: 100 }}>
       <h1>🛡️ MA Guard</h1>
-      <p>Сила не в хаосе, а в направлении</p>
+      <p>💬Сила не в хаосе, а в направлении</p>
     </main>
   );
 }
